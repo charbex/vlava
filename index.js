fc7a18755e7047010a5a82dc6600e96f33ddb3da
char <charbel@hotmail.com>
2024-03-28 00:07:14 +0200

    third commit

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 
- var _servicescontainer=document.getElementById('servicescontainer')
- for (let i = 0; i < 5; i++) {
-    let _icon='briefcase';
-    if(i==1){
-        _icon='map'
-    }
-    if(i==2){
-        _icon='envelope'
-    }
-    var content=`
-     
-               <div class="icon"><i class="bi bi-${_icon}"></i></div>
-               <h4 class="title"><a href="">Lorem Ipsum ${i}</a></h4>
-               <p class="description">Voluptatubbm deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
-              
-    ` 
-   let _div= document.createElement('div');
-   _div.innerHTML=content;
-   _div.className="col-lg-4 col-md-6 icon-box"
-   _servicescontainer.appendChild(_div);
- }
+ var _servicescontainer=document.getElementById('servicescontainer');
+ fetch('http://127.0.0.1:3000/services')
+ .then(response => response.json())
+ .then(json =>{
+    //alert (json);
+    for (let i = 0; i <json.length; i++) {
+        let _icon='briefcase';
+        if(i==1){
+            _icon='map'
+        }
+        if(i==2){
+            _icon='envelope'
+        }
+        var content=`
+         
+                   <div class="icon"><i class="bi bi-${json[i].icon}"></i></div>
+                   <h4 class="title"><a href="">${json[i].title}</a></h4>
+                   <p class="description">Voluptatubbm deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+                  
+        ` 
+       let _div= document.createElement('div');
+       _div.innerHTML=content;
+       _div.className="col-lg-4 col-md-6 icon-box"
+       _servicescontainer.appendChild(_div);
+     } 
+ })
+
+ 
